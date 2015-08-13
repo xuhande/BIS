0f87cb794fbeee085cd9bc4a1be6245ce72e5fda
xuhande <hande.xu@pixitek.com>
2015-08-13 16:24:02 +0800

    20150813-1 xhd update data

--- app/controllers/Contact.scala
+++ app/controllers/Contact.scala
@@ -18,14 +18,13 @@ import play.api.data.Form
 import play.api.data.Forms.tuple
 import play.api.data.Forms.text
 import play.api.i18n.Messages
-
+import org.apache.commons.mail.EmailAttachment
+import com.typesafe.plugin._
+import play.api.Play.current
+import play.api.Play
+import play.Logger
 object Contact extends Controller  {
-//  case class Messages(name: String,email:String)
 val MessageForm = Form(
-  //    mapping(
-//  "name" -> text,
-//  "email" -> text
-  //    )(Messages.apply, Messages.unapply)
     tuple(
   "name" -> text,
   "email" -> text,
@@ -42,14 +41,19 @@ val MessageForm = Form(
 
   def sendMail = Action { implicit request =>
   val (name, email,message) = MessageForm.bindFromRequest.get
-          val s = new models.mail.Mail (
-            from = ("xuboy1@126.com", "225836"),
-            to = Seq("hande.xu@yzp2p.com"),
-            subject = "test email",
-            message = "test email"
-          )
-  println(s)
+    receiptMessage(name,email,message)
         Redirect(routes.Contact.index).flashing("Success" -> Messages("您已成功发送邮件给我们了，请您留意您的邮箱."))
+  }
+
+
+
+  def receiptMessage(name:String,email:String,Message:String) =  {
+
+val mail = use[MailerPlugin].email
+    mail.setSubject("Vycos 联系消息")
+    mail.addRecipient("Vycos <hande.xu@pixitek.com>") //发送到目的地址<请修改接收地址>
+    mail.addFrom("Vycos <xuboy1@126.com>") //系统发送的邮箱,必须与conf邮箱一致
+    mail.send("","<html><head><title>"+name+"</title></head><body><strong>客户名称：</strong>"+name+"<br /><br /><strong>客户邮箱：</strong>"+email+"<br /><br /><strong>留言内容：</strong>v"+Message+"</body>")  //  以html方式发送邮件
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -6,7 +6,7 @@ libraryDependencies ++= Seq(
   //jdbc,
   //anorm,
   cache,
-  "org.apache.commons" % "commons-email" % "1.3.2"
+//  "org.apache.commons" % "commons-email" % "1.3.2"
   //"com.typesafe.play" %% "play-slick" % "0.6.0.1",
   //"com.typesafe.slick" %% "slick" % "2.0.1",
   //"com.typesafe.slick" %% "slick-extensions" % "2.0.1",
@@ -15,7 +15,8 @@ libraryDependencies ++= Seq(
 //  "postgresql" % "postgresql" % "9.3-1101.jdbc41"
 //  "org.postgresql" % "postgresql" % "9.3-1101.jdbc41"
   //"org.postgresql" % "postgresql" % "9.3-1100-jdbc41"
-)     
+"com.typesafe" %% "play-plugins-mailer" % "2.1-RC2"
+)
 
 //resolvers ++= Seq(
 //  "Typesafe Releases" at "http://repo.typesafe.com/typesafe/maven-releases/",
